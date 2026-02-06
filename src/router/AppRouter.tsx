d418fa6211f2990a66cf2abec66f25a034f7b295
Jaewook Kim <yearsingle@naver.com>
2026-02-07 05:07:18 +0900

    hotfix: reload issus

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import App from "../App";
 import DashboardPage from "../pages/DashboardPage";
 import LoginPage from "../pages/LoginPage";
@@ -8,10 +8,8 @@ import WordDetailPage from "../pages/WordDetailPage";
 import RequireAuth from "./RequireAuth";
 
 export default function AppRouter() {
-  const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
-
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
@@ -25,6 +23,6 @@ export default function AppRouter() {
         </Route>
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
